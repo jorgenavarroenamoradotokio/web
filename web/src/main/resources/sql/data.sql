c13091a16b8edd095d0376eae357b0d4ccd0328a
juan_ <juan_@DESKTOP-PRRQ0C0.home>
2022-07-26 18:56:56 +0200

    commit sql ejecucion automatica

--- web/src/main/resources/sql/data.sql
+++ web/src/main/resources/sql/data.sql
@@ -1,1 +1,2 @@
-insert into roles (nombre) values ("PROFILE_TEST");
+insert into roles values (1000,"ADMIN");
+INSERT INTO filmotokio.users values('1000',1, now(),now(),"email@email.com",null,"test","$2a$10$9Wqtwh4oaIUboZRc9xK12uJB30S0nqLm.tcrKX4JjWYl3bmRzdizK","test","testSQL",1000);
